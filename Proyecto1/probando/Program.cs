b8aef22770c73e9b2f9162bca24bc9ab1d54d982
sarismejiasanchez <sarismejiasanchez@gmail.com>
2025-02-11 16:06:52 -0500

    Add Stripe API key for testing in Program.cs

--- Proyecto1/probando/Program.cs
+++ Proyecto1/probando/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
+
+// Stripe API Key for testing
+string stripeApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
